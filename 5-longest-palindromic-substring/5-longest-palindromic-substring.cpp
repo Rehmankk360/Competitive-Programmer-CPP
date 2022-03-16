7ae763a183943ef2a91019a6ac3f7d3bf64332f6
Rehman Khan <66064740+Shutah64@users.noreply.github.com>
2022-03-16 22:38:24 +0500

    Time: 340 ms (34.75%), Space: 25.6 MB (42.21%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -7,14 +7,11 @@ public:
         if (n == 0)
             return "";
 
-        // dp[i][j] will be 'true' if the string from index i to j is a palindrome.
         bool dp[n][n];
 
-        //Initialize with false
 
         memset(dp, 0, sizeof(dp));
 
-        //Every Single character is palindrome
         for (int i = 0; i < n; i++)
             dp[i][i] = true;
 
@@ -27,13 +24,10 @@ public:
             {
                 if (s[i] == s[j])
                 {
-                    //If it is of two character OR if its susbtring is palindrome.
                     if (j - i == 1 || dp[i + 1][j - 1])
                     {
-                        //Then it will also a palindrome substring
                         dp[i][j] = true;
 
-                        //Check for Longest Palindrome substring
                         if (ans.size() < j - i + 1)
                             ans = s.substr(i, j - i + 1);
                     }
