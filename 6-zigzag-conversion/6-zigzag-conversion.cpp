e3b00422aab6d993f71dbf528ea79a6050258169
Rehman Khan <66064740+Shutah64@users.noreply.github.com>
2022-03-16 22:53:00 +0500

    Time: 15 ms (57.57%), Space: 10.6 MB (47.64%) - LeetHub

---
+++ 6-zigzag-conversion/6-zigzag-conversion.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    string convert(string s, int numRows) 
+    {
+        if (numRows <= 1) return s;
+        vector<string> zigzag(numRows, "");
+        int len = s.size();
+        int state = 0, row = 0;
+        for (int i = 0; i < len;)
+        {
+            if (state == 0) 
+            {
+                // go down
+                zigzag[row++] += s[i++];
+                if (row == numRows)
+                {
+                    row = numRows - 2;
+                    state = 1; // go up
+                }
+            } 
+            else
+            {
+                // state == 1
+                // go up
+                // row from 1 to numRows - 2
+                if (row >= 1)
+                {
+                    zigzag[row--] += s[i++];
+                    if (row == 0) state = 0;
+                } 
+                else
+                {
+                    row = 0;
+                    state = 0;
+                }
+            }
+        }
+        string ans;
+        for(auto a: zigzag) ans += a;
+        return ans;
+    }
+};
