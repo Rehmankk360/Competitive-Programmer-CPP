5689ba065d51ce24310ce3f1fb3f080d10946333
Rehman Khan <66064740+Shutah64@users.noreply.github.com>
2022-08-31 00:44:20 +0500

    Time: 1067 ms (20.31%), Space: 176.9 MB (59.16%) - LeetHub

---
+++ 2349-design-a-number-container-system/2349-design-a-number-container-system.cpp
@@ -0,0 +1,26 @@
+class NumberContainers {
+public:
+    unordered_map<int,set<int>>map_nums; //(number,{index}) (used set in place of unordered set to get minimum quickly )
+    unordered_map<int,int>map_index;// (index,number)
+    NumberContainers() {
+        
+    }
+    void change(int index, int number) {
+         //if index already exists we have to change the map_index at given "index"
+         if(map_index.find(index)!=map_index.end())
+         {
+             int num=map_index[index];  
+             if(map_nums[num].size()) map_nums[num].erase(index);
+             if(map_nums[num].size()==0)
+                 map_nums.erase(num);
+         }
+          map_index[index]=number;
+            map_nums[number].insert(index);    
+    }
+    
+    int find(int number) {
+        if(map_nums.find(number)==map_nums.end())
+            return -1;
+        return *map_nums[number].begin();
+    }
+};
