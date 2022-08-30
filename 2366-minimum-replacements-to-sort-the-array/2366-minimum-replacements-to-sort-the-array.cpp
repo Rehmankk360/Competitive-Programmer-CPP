22140d631982a43fc3caa373c190f6c4af8a4791
Rehman Khan <66064740+Shutah64@users.noreply.github.com>
2022-08-30 23:29:07 +0500

    Time: 112 ms (99.69%), Space: 54.7 MB (61.46%) - LeetHub

--- 2366-minimum-replacements-to-sort-the-array/2366-minimum-replacements-to-sort-the-array.cpp
+++ 2366-minimum-replacements-to-sort-the-array/2366-minimum-replacements-to-sort-the-array.cpp
@@ -6,19 +6,18 @@ public:
      int prev = 1e9+7;
      for(int i=n-1;i>=0;i--)
      {
-        // Here count denotes that no. of components after breaking 
         int count ;
-        if(nums[i]%prev==0)  // Like 5 5 => count=1 => operation=0
+        if(nums[i]%prev==0)  
         {
             count = nums[i]/prev;
             prev = nums[i]/count;
         }
-        else if(nums[i]%prev!=0)  //  case 1: 5 7 => count=1 => operation=0
-        {                         //  case 2: 9 5 => count=2 => operation=1
+        else if(nums[i]%prev!=0)  
+        {                         
           count = (nums[i]+prev-1)/prev;
           prev = nums[i]/count;
         }
-        operation += count-1; // for 'count' components we need minimum count-1 operation
+        operation += count-1;
      }
      return operation;
     }
